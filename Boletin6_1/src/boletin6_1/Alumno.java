1ab6c5c505253f7105faf7e238f26a3955ce8be3
aparcerozas <aparcerozas@victoria11.danielcastelao.org>
2018-11-08 09:12:07 +0100

    C

---
+++ Boletin6_1/src/boletin6_1/Alumno.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin6_1;
+
+/**
+ *
+ * @author aparcerozas
+ */
+public class Alumno {
+    private String nombre;
+    private String dni;
+    private String telefono;
+    private int edad;
+    
+    public Alumno(){}
+    
+}
